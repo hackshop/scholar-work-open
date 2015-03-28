19ec6412a4360aec82212d10224be28da2b4dcf9
BSF STEM Scholar <stemscholar.bsf@gmail.com>
2015-03-28 09:58:51 -0500

    we_did_it

---
+++ sketchbook/Group_Unicorns_FTW/we_did_it/we_did_it.ino
@@ -0,0 +1,23 @@
+
+void setup(){
+
+pinMode(13, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(9, OUTPUT);
+}
+void loop() {
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(1000);    
+digitalWrite(11, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(11, LOW);    // set the LED off
+  delay(1000); 
+digitalWrite(9, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(9, LOW);    // set the LED off
+  delay(1000);              // w  // w  // wait for a second
+}
+
+
