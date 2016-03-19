7c81123e98a3da1855fbc23bfade8d3a0d083977
BSF STEM Scholar <stemscholar.bsf@gmail.com>
2016-03-19 10:51:34 -0500

    Hackshop Challenge 5.5

--- sketchbook/Blink/Blink.ino
+++ sketchbook/Blink/Blink.ino
@@ -5,15 +5,27 @@
   This example code is in the public domain.
  */
 
-void setup() {                
+void setup() {             
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(13, OUTPUT);     
+  pinMode(7, OUTPUT);     
 }
 
 void loop() {
-  digitalWrite(13, HIGH);   // set the LED on
-  delay(2000);              // wait for a second
-  digitalWrite(13, LOW);    // set the LED off
-  delay(500);              // wait for a second
-}
+  digitalWrite(7, HIGH);   // set the LED on
+  delay(20);              // wait for a second
+  digitalWrite(7, LOW);    // set the LED off
+  delay(30);              // wait for a second
+  digitalWrite(7, HIGH);   // set the LED on
+  delay(20);              // wait for a second
+  digitalWrite(7, LOW);    // set the LED off
+  digitalWrite(7, HIGH);   // set the LED on
+  delay(50);              // wait for a second
+  digitalWrite(7, LOW);    // set the LED off
+  delay(20);              // wait for a second
+  digitalWrite(7, HIGH);   // set the LED on
+  delay(60);              // wait for a second
+  digitalWrite(7, LOW);    // set the LED off
+  delay(20);              // wait for a second
+}  
+
